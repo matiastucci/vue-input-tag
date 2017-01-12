53c05cb8095fce09e03efa05782f9fec9b223b75
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-01-12 18:14:14 +0100

    #10, added README, demo and method code

--- src/InputTag.vue
+++ src/InputTag.vue
@@ -1,4 +1,13 @@
 <script>
+/*eslint-disable*/
+  const validators = {
+    email : new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+    url : new RegExp(/^(https?|ftp|rmtp|mms):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i),
+    text : new RegExp(/^[a-zA-Z]+$/),
+    digits : new RegExp(/^[\d() \.\:\-\+#]+$/),
+    isodate : new RegExp(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/)
+  }
+  /*eslint-enable*/
   export default {
     name: 'InputTag',
 
@@ -18,25 +27,35 @@
         type: Boolean,
         default: false,
       },
+      validate: {
+        type: String,
+        default: '',
+      },
     },
-
     data() {
       return {
         newTag: '',
       };
     },
-
     methods: {
       focusNewTag() {
         if (this.readOnly) { return; }
         this.$el.querySelector('.new-tag').focus();
       },
       addNew(tag) {
-        if (tag && !this.tags.includes(tag)) {
+        if (tag && !this.tags.includes(tag) && this.validateIfNeeded(tag)) {
           this.tags.push(tag);
           this.tagChange();
         }
         this.newTag = '';
+      },
+      validateIfNeeded(tagValue) {
+        if (this.validate === '' || this.validate === undefined) {
+          return true;
+        } else if (Object.keys(validators).indexOf(this.validate) > -1) {
+          return validators[this.validate].test(tagValue);
+        }
+        return true;
       },
       remove(index) {
         this.tags.splice(index, 1);
